19f88810b6f027fc441c71564af321496f0cfba7
Nicholas Sharp <naas8905@gmail.com>
2020-08-07 16:03:20 -0700

    Adding class based views

--- moviesapp/movies/views.py
+++ moviesapp/movies/views.py
@@ -14,12 +14,15 @@ from .models import Movie
 class MovieListView(ListView):
     """Show all movies."""
 
-    # model = Movie
+    model = Movie
     template_name = 'movies/movie_list.html'
    
-
-    def get_queryset(self):
-        return Movie.objects.order_by('-year')
+    def get_queryset(self, *args, **kwargs): 
+        qs = super(MovieListView, self).get_queryset(*args, **kwargs) 
+        qs = qs.order_by("-year") 
+        return qs 
+    # def get_queryset(self):
+    #     return Movie.objects.order_by('-year')
     
 
 
@@ -29,16 +32,22 @@ class MovieDetailView(DetailView):
     model = Movie
     template_name = 'movies/movie_detail.html'
 
+    def get_movie_details(self, *args, **kwargs):
+        context = super(MovieDetailView, self).get_movie_details(*args, **kwargs)
+        return context
+
 
 class MovieCreateView(CreateView):
     """Create a new movie."""
     model = Movie
+    fields = ["title", "year", "rated", "realeased_on", "genre", "director", "plot"]
     template_name = 'movies/movie_form.html'
 
 
 class MovieUpdateView(UpdateView):
     """Update the requested movie."""
     model = Movie
+    fields = ["title", "year", "rated", "realeased_on", "genre", "director", "plot"]
     template_name = 'movies/movie_form.html'
 
 
