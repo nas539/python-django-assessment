6068f18106b755c61877421a5edbdf1e0e4a5b99
Nicholas Sharp <naas8905@gmail.com>
2020-08-07 15:04:52 -0700

    Fixed app_name urls.py

--- moviesapp/movies/urls.py
+++ moviesapp/movies/urls.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
-movies_patterns = ([
-   path('', view=views.MovieListView.as_view(), name='index'),
-    path('(<int:id>/',
+app_name = 'movies'
+urlpatterns = [
+    path('', view=views.MovieListView.as_view(), name='index'),
+    path('(<int:pk>/',
          view=views.MovieDetailView.as_view(), name='detail'),
     path('create/', view=views.MovieCreateView.as_view(), name='create'),
-    path('update/<int:id>/',
+    path('update/<int:pk>/',
          view=views.MovieUpdateView.as_view(), name='update'),
-    path('delete/<int:id>/',
-         view=views.MovieDeleteView.as_view(), name='delete')
-], 'movies')
-
-urlpatterns = [
-    path('movies/', include(movies_patterns)),
+    path('delete/<int:pk>/',
+         view=views.MovieDeleteView.as_view(), name='delete'),
 ]
